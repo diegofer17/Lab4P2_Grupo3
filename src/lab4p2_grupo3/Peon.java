ac67a8a29e7ac535a8239915b8722cf2057c581f
Marcela Tovar <marcelatovar.flores@gmail.com>
2023-02-10 15:27:39 -0600

    Creacion de las clases

---
+++ src/lab4p2_grupo3/Peon.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab4p2_grupo3;
+
+/**
+ *
+ * @author marcela
+ */
+public class Peon extends Pieza {
+
+    @Override
+    public boolean movimiento(int X1, int Y1, int X2, int Y2, Object[][] tablero) {
+        boolean check = true;
+        Object tab = tablero[X2][Y2];
+        if (super.movimiento(X1, Y1, X2, Y2, tablero)) {
+            if (X2 == X1 && Y2 != Y1 || X2 == X1 + 1) {
+                check = true;
+            } else {
+                check = false;
+            }
+        }
+        return check;
+    }
+}
