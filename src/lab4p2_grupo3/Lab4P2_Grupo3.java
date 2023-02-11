492896f15973e1c6ec033d0a5843569481db4914
Marcela Tovar <marcelatovar.flores@gmail.com>
2023-02-10 18:47:16 -0600

    Imprime

--- src/lab4p2_grupo3/Lab4P2_Grupo3.java
+++ src/lab4p2_grupo3/Lab4P2_Grupo3.java
@@ -3,55 +3,46 @@ package lab4p2_grupo3;
 import java.util.Scanner;
 
 public class Lab4P2_Grupo3 {
-    
+
     static Scanner leer = new Scanner(System.in);
-    static Object [][] tablero = new Object[9][9];
+    static Object[][] tablero = new Object[9][9];
+
     public static void main(String[] args) {
-        
-        
+
         System.out.println("Juego de ajedrez\n"
                 + "\n");
         System.out.print("Ingrese el nombre del jugador de piezas blancas: ");
         String jugador1 = leer.next();
         System.out.print("Ingrese el nombre del jugador de piezas negras: ");
         String jugador2 = leer.next();
-        
+
         System.out.println("Tablero");
         System.out.println("\n");
-        llenarMatriz(tablero);
+        llenarMatriz(tablero, jugador1, jugador2);
         imprimir(tablero);
     }
-    
-    public Object [][] creartablero(int filas, int cols){
-        Object [][] temporal = new Object [filas][cols];
-        
-        temporal[0][0]= new Pieza(0, 0, true, "r");
-        temporal[0][1]= new Pieza(0, 1, true, "n");
-        temporal[0][2]= new Pieza(0, 2, true, "b");
-        temporal[0][3]= new Pieza(0, 3, true, "k");
-        temporal[0][4]= new Pieza(0, 4, true, "q");
-        temporal[0][5]= new Pieza(0, 5, true, "b");
-        temporal[0][6]= new Pieza(0, 6, true, "n");
-        temporal[0][7]= new Pieza(0, 7, true, "r");
-        
+
+    public Object[][] creartablero(int filas, int cols) {
+        Object[][] temporal = new Object[filas][cols];
+
         for (int i = 0; i < temporal.length; i++) {
             for (int j = 0; j < temporal[i].length; j++) {
                 if (i == 2) {
-                    
+
                 }
             }
         }
-        
+
         for (int i = 0; i < temporal.length; i++) {
             for (int j = 0; j < temporal[i].length; j++) {
-                if(temporal[i][j]==null){
-                    temporal [i][j]= "[ ]";
+                if (temporal[i][j] == null) {
+                    temporal[i][j] = "[ ]";
                 }
             }
         }
         return temporal;
     }
-    
+
     public static void imprimir(Object[][] tablero) {
         /*String cadena = "";
         
@@ -63,50 +54,108 @@ public class Lab4P2_Grupo3 {
             cadena += "\n";
         }*/
         System.out.println("");
-        
-        for(int i = 0; i < tablero.length; i += 1) {
+
+        for (int i = 0; i < tablero.length; i += 1) {
             System.out.println("");
-            System.out.println("---------------------");
-            for(int j = 0; j < tablero[i].length; j += 1) {
+            for (int j = 0; j < tablero[i].length; j += 1) {
                 System.out.print("| " + tablero[i][j] + " ");
-                if(j == tablero[i].length - 1) {
-                    System.out.print("|"+i);
+                if (j == tablero[i].length - 1) {
+                    System.out.print("|" + i);
                 }
             }
         }
-        
+
         System.out.println();
     }//Fin metodo imprimir
-    
-    public static Object[][] llenarMatriz(Object[][] tablero){
+
+    public static Object[][] llenarMatriz(Object[][] tablero, String jugador1, String jugador2) {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                if (i == 1 && j == 0) {
-                    Peon p = new Peon(0,0,true,"p");
-                    tablero[1][0] = p;
-                }else if(i == 1 && j == 1){
-                    Peon p = new Peon(0,0,true,"p");
-                    tablero[1][1] = p;
-                }else if(i == 1 && j == 2){
-                    Peon p = new Peon(0,0,true,"p");
-                    tablero[1][2] = p;
-                }else if(i == 1 && j == 3){
-                    Peon p = new Peon(0,0,true,"p");
-                    tablero[1][3] = p;
-                }else if(i == 1 && j == 4){
-                    Peon p = new Peon(0,0,true,"p");
-                    tablero[1][4] = p;
-                }else if(i == 1 && j == 5){
-                    Peon p = new Peon(0,0,true,"p");
-                    tablero[1][5] = p;
-                }else if(i == 1 && j == 6){
-                    Peon p = new Peon(0,0,true,"p");
-                    tablero[1][6] = p;
-                }else if(i == 1 && j == 7){
-                    Peon p = new Peon(0,0,true,"p");
-                    tablero[1][7] = p;
-                }
+                Torre t = new Torre(0, 0, 1, "R");
+                tablero[0][0] = t;
+
+                Torre t2 = new Torre(0, 0, 1, "R");
+                tablero[0][7] = t2;
+            
+                Torre t3 = new Torre(0, 0, 2, "r");
+                tablero[7][0] = t3;
+            
+                Torre t4 = new Torre(0, 0, 1, "r");
+                tablero[7][7] = t4;
+           
+                Caballo c = new Caballo(0, 0, 1, "N");
+                tablero[0][1] = c;
+            
+                Caballo c1 = new Caballo(0, 0, 1, "N");
+                tablero[0][6] = c1;
+            
+                Caballo c2 = new Caballo(0, 0, 2, "n");
+                tablero[7][1] = c2;
+            
+                Caballo c3 = new Caballo(0, 0, 2, "n");
+                tablero[7][6] = c3;
+            
+                Alfil a = new Alfil(0, 0, 1, "B");
+                tablero[0][2] = a;
+            
+                Alfil a2 = new Alfil(0, 0, 1, "B");
+                tablero[0][5] = a2;
+            
+                Alfil a3 = new Alfil(0, 0, 2, "b");
+                tablero[7][2] = a3;
+            
+                Alfil a5 = new Alfil(0, 0, 2, "b");
+                tablero[7][5] = a5;
+            
+                Rey r = new Rey(0, 0, 1, "K");
+                tablero[0][4] = r;
+            
+                Rey r1 = new Rey(0, 0, 2, "k");
+                tablero[7][4] = r1;
+            
+                Dama r2 = new Dama(0, 0, 1, "Q");
+                tablero[0][3] = r2;
+            
+                Dama r3 = new Dama(0, 0, 2, "q");
+                tablero[7][3] = r3;
+                
+                Peon p1 = new Peon(0, 0, 1, "P");
+                tablero[2][1] = p1;
+                Peon p2 = new Peon(0, 0, 1, "P");
+                tablero[2][2] = p2;
+                Peon p3 = new Peon(0, 0, 1, "P");
+                tablero[2][3] = p3;
+                Peon p4 = new Peon(0, 0, 1, "P");
+                tablero[2][4] = p4;
+                Peon p5 = new Peon(0, 0, 1, "P");
+                tablero[2][5] = p5;
+                Peon p6 = new Peon(0, 0, 1, "P");
+                tablero[2][6] = p6;
+                Peon p7 = new Peon(0, 0, 1, "P");
+                tablero[2][7] = p7;
+                Peon p8 = new Peon(0, 0, 1, "P");
+                tablero[2][8] = p8;
+                
+                Peon p11 = new Peon(0, 0, 2, "p");
+                tablero[7][1] = p11;
+                Peon p22 = new Peon(0, 0, 2, "p");
+                tablero[7][2] = p22;
+                Peon p33 = new Peon(0, 0, 2, "p");
+                tablero[7][3] = p33;
+                Peon p44 = new Peon(0, 0, 2, "p");
+                tablero[7][4] = p44;
+                Peon p55 = new Peon(0, 0, 2, "p");
+                tablero[7][5] = p55;
+                Peon p66 = new Peon(0, 0, 2, "p");
+                tablero[7][6] = p66;
+                Peon p77 = new Peon(0, 0, 2, "p");
+                tablero[7][7] = p77;
+                Peon p88 = new Peon(0, 0, 2, "p");
+                tablero[7][8] = p88;
+                
             }
+
+            
         }
         return tablero;
     }
